67c6068f5a18e7adebbface678afd3dd5cecc485
electrohyun <dev.electrohyun@gmail.com>
2026-06-10 23:11:25 +0900

    feat: add import-spacing rule

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import importSpacingRule from "./rules/import-spacing";
+
 const plugin = {
   meta: {
     name: "@electrohyun/eslint-plugin-layered-imports",
     version: "0.0.0",
   },
-  rules: {},
+  rules: {
+    "import-spacing": importSpacingRule,
+  },
 };
 
 export default plugin;
