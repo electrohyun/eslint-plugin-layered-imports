f4329a9fea1f1940c1c93c002015ece0b6e0724a
Gihyeon Jeong / 정기현 <dev.electrohyun@gmail.com>
2026-06-12 00:01:36 +0900

    feat: add fsd-friendly preset (#17)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,44 @@
+import { Linter, Rule } from "eslint";
 import importSpacingRule from "./rules/import-spacing";
 
-const plugin = {
+interface LayeredImportsPlugin {
+  meta: {
+    name: string;
+    version: string;
+  };
+  rules: {
+    "import-spacing": Rule.RuleModule;
+  };
+  configs: {
+    fsd: Linter.Config;
+  };
+}
+
+const plugin: LayeredImportsPlugin = {
   meta: {
     name: "@electrohyun/eslint-plugin-layered-imports",
     version: "0.0.0",
   },
   rules: {
     "import-spacing": importSpacingRule,
+  },
+  configs: {
+    fsd: {} as Linter.Config,
+  },
+};
+
+plugin.configs.fsd = {
+  plugins: {
+    "layered-imports": plugin,
+  },
+  rules: {
+    "layered-imports/import-spacing": [
+      "error",
+      {
+        internalAliases: ["@/"],
+        groups: ["builtin", "external", "internal", "relative"],
+      },
+    ],
   },
 };
 
