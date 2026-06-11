119f39ee8858bc5856684a30e71c89b9902a2d72
electrohyun <dev.electrohyun@gmail.com>
2026-06-11 16:57:48 +0900

    feat: add import-spacing autofix

--- src/rules/import-spacing.test.ts
+++ src/rules/import-spacing.test.ts
@@ -14,14 +14,61 @@ const validExternalToInternal = [
   'import { Button } from "@/shared/ui";',
 ].join("\n");
 
+const validAllInitialGroups = [
+  'import path from "node:path";',
+  "",
+  'import React from "react";',
+  "",
+  'import { Button } from "@/shared/ui";',
+  "",
+  'import helper from "./helper";',
+].join("\n");
+
+const validSameBuiltinGroup = [
+  'import path from "node:path";',
+  'import fs from "fs";',
+].join("\n");
+
+const validBuiltinToExternal = [
+  'import path from "node:path";',
+  "",
+  'import React from "react";',
+].join("\n");
+
 const invalidExternalToInternal = [
   'import React from "react";',
   'import { Button } from "@/shared/ui";',
 ].join("\n");
 
+const invalidBuiltinToExternal = [
+  'import path from "node:path";',
+  'import React from "react";',
+].join("\n");
+
+const invalidInternalToRelative = [
+  'import { Button } from "@/shared/ui";',
+  'import helper from "./helper";',
+].join("\n");
+
+const validInternalToRelative = [
+  'import { Button } from "@/shared/ui";',
+  "",
+  'import helper from "./helper";',
+].join("\n");
+
 const validSameExternalGroup = [
   'import React from "react";',
   'import { clsx } from "clsx";',
+].join("\n");
+
+const validSameInternalGroup = [
+  'import { userApi } from "@/entities/user";',
+  'import { Button } from "@/shared/ui";',
+].join("\n");
+
+const validSameRelativeGroup = [
+  'import helper from "./helper";',
+  'import util from "../util";',
 ].join("\n");
 
 ruleTester.run("import-spacing", rule, {
@@ -30,13 +77,36 @@ ruleTester.run("import-spacing", rule, {
       code: validExternalToInternal,
     },
     {
+      code: validAllInitialGroups,
+    },
+    {
+      code: validSameBuiltinGroup,
+    },
+    {
       code: validSameExternalGroup,
+    },
+    {
+      code: validSameInternalGroup,
+    },
+    {
+      code: validSameRelativeGroup,
     },
   ],
 
   invalid: [
     {
+      code: invalidBuiltinToExternal,
+      output: validBuiltinToExternal,
+      errors: [{ messageId: "missingBlankLine" }],
+    },
+    {
       code: invalidExternalToInternal,
+      output: validExternalToInternal,
+      errors: [{ messageId: "missingBlankLine" }],
+    },
+    {
+      code: invalidInternalToRelative,
+      output: validInternalToRelative,
       errors: [{ messageId: "missingBlankLine" }],
     },
   ],
